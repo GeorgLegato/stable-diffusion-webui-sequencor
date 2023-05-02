68b62526d0b75a6ca5d6baa3d754813284d0c489
GeorgLegato <bulbul@onlinehome.de>
2023-05-03 00:45:58 +0200

    fixed 2nd run path setting

--- scripts/processors/rife_cli_adapter.py
+++ scripts/processors/rife_cli_adapter.py
@@ -6,6 +6,7 @@ from types import SimpleNamespace
 from modules import scripts
 
 usefulDirs = scripts.basedir().split(os.sep)[-2:]  # contains install and our extension foldername
+isInCDW = False
 
 def process(im1: Image,im2: Image, steps: int, ffprobepath: str):
     
@@ -24,8 +25,7 @@ def process(im1: Image,im2: Image, steps: int, ffprobepath: str):
     im1.save(os.path.join(stepfolders[0], "00000001.png"), "PNG")  # save image to temp dir for processing to work.
     im2.save(os.path.join(stepfolders[0], "00000002.png"), "PNG")  # save image to temp dir for processing to work.
 
-    path = f"{usefulDirs[0]}/{usefulDirs[1]}/scripts/processors/RIFE"
-    os.chdir(path)
+    set_cwd()
 
     for i in range(steps-1):
         callRIFE(stepfolders[i],stepfolders[i+1])
@@ -35,12 +35,18 @@ def process(im1: Image,im2: Image, steps: int, ffprobepath: str):
     return outv   # return path to output.mp4 file.
  
  
+def set_cwd():
+    global isInCDW
+    path = f"{usefulDirs[0]}/{usefulDirs[1]}/scripts/processors/RIFE"
+    if not isInCDW:
+        os.chdir(path)
+        isInCDW=True
+ 
 def callRIFE(folder1: str, folder2: str):
     os.system(f"rife-ncnn-vulkan.exe -i {folder1} -o  {folder2} -m rife-v4.6")
 
 def createVideo(infolder, outfile):
     os.system(f"ffmpeg -framerate 30 -i {infolder}\%08d.png -c:a copy -crf 20 -c:v libx264 -pix_fmt yuv420p {outfile}")
-
 
 def fixEnvPath(p):
     envpath = os.environ["PATH"]
